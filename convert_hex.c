e8ec70a511abebc6e90dc7ab75f41d957b8252fe
Temitope Aderemi Olamolu <72602757+t-olamolu@users.noreply.github.com>
2021-10-03 19:54:09 +0100

    Update convert_hex.c

--- convert_hex.c
+++ convert_hex.c
@@ -6,10 +6,6 @@
 
 #include "main.h"
 
-unsigned int convert_x(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_X(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
 
 /**
  * convert_x - Converts an unsigned int argument to hex using abcdef
