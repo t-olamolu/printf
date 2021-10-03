fe0047089d05e3710c18e4be46a2f85675b8915f
Temitope Aderemi Olamolu <72602757+t-olamolu@users.noreply.github.com>
2021-10-03 19:42:35 +0100

    Update memory_helpers.c

--- memory_helpers.c
+++ memory_helpers.c
@@ -1,9 +1,5 @@
 #include "main.h"
 
-unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n);
-void free_buffer(buffer_t *output);
-buffer_t *init_buffer(void);
-
 /**
  * _memcpy - Copies n bytes from memory area src to
  *           the buffer contained in a buffer_t struct.
