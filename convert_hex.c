0f9b5fba31117bfa0f0f5f0b4e13fb85cb3d0cb6
Temitope Aderemi Olamolu <72602757+t-olamolu@users.noreply.github.com>
2021-10-03 18:35:15 +0100

    Update convert_hex.c

--- convert_hex.c
+++ convert_hex.c
@@ -4,7 +4,7 @@
  *       Shaldas Homsuk
  */
 
-#include "holberton.h"
+#include "main.h"
 
 unsigned int convert_x(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
